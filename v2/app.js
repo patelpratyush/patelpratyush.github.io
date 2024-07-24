f02a7f37acc8b41d10312fe0879449a2c3231d1c
Pratyush Patel <patelpratyush28@gmail.com>
2024-07-24 17:40:40 -0400

    update app.js

--- v2/app.js
+++ v2/app.js
@@ -1,38 +1,35 @@
-//template_0l39ewb
-//service_5pfyxcu
-//OGVq5jbZr4x4aaLdm
+// Variables and Functions
+
 let isModalOpen = false;
-let contrastToggle = false;
+let contrastToggle = true; // Default to dark mode
 const scaleFactor = 1 / 20;
 
 function moveBackground(event) {
-    const shapes = document.querySelectorAll(".shape"); //this returns an array
+    const shapes = document.querySelectorAll(".shape");
     const x = event.clientX * scaleFactor;
     const y = event.clientY * scaleFactor;
 
     for (let i = 0; i < shapes.length; i++) {
         const isOdd = i % 2 !== 0;
-        const boolInt = isOdd ? -1 : 1; //if isOdd is true, its -1, if not its 1
+        const boolInt = isOdd ? -1 : 1;
         shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
     }
 }
 
 function toggleContrast() {
-    contrastToggle = !contrastToggle
+    contrastToggle = !contrastToggle;
     if (contrastToggle) {
-        document.body.classList += " dark-theme" //this adds another class onto the body
-
+        document.body.classList.add("dark-theme");
     } else {
-        document.body.classList.remove("dark-theme")
+        document.body.classList.remove("dark-theme");
     }
 }
-
 
 function contact(event) {
     event.preventDefault();
     const loading = document.querySelector(".modal__overlay--loading");
     const success = document.querySelector(".modal__overlay--success");
-    loading.classList += " modal__overlay--visible"
+    loading.classList.add("modal__overlay--visible");
     emailjs
         .sendForm(
             "sservice_x3rcr8q",
@@ -41,26 +38,29 @@ function contact(event) {
             "flrgfS_1a5y35U1C1"
         ).then(() => {
             loading.classList.remove("modal__overlay--visible");
-            success.classList += " modal__overlay--visible";
+            success.classList.add("modal__overlay--visible");
         }).catch(() => {
             loading.classList.remove("modal__overlay--visible");
             alert(
                 "The email service is temporarily unavailable. Please contact me directly on patelpratyush28@gmail.com"
             );
-        })
+        });
 }
-
-
 
 function toggleModal() {
-    
     if (isModalOpen) {
         isModalOpen = false;
-        return document.body.classList.remove("modal--open")
+        document.body.classList.remove("modal--open");
+    } else {
+        isModalOpen = true;
+        document.body.classList.add("modal--open");
     }
-    isModalOpen = true;
-    //toggle the modal
-    document.body.classList += " modal--open"
-    
 }
 
+// Initialize
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (contrastToggle) {
+        document.body.classList.add("dark-theme");
+    }
+});
